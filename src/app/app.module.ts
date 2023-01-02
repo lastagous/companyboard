4f8962c7a5455799781f5948cef72d8ac7a44006
lastagous <from.iphone5s@gmail.com>
2023-01-02 11:14:58 +0900

    [FIX] header と footer のファイル移動

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './page/header/header.component';
-import { FooterComponent } from './page/footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
