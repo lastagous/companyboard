2cc9b310e8e782e562a6e60ae9d1a48f6bc03dd0
lastagous <from.iphone5s@gmail.com>
2023-01-06 23:05:52 +0900

    [FIX] header と footer を component の配下に移動

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 import { TopComponent } from './page/top/top.component';
 import { MembersComponent } from './page/members/members.component';
 
