8f69cebcadeace1876edfeb8b5b771232f8ade13
lastagous <from.iphone5s@gmail.com>
2023-01-15 14:11:30 +0900

    [FIX] prod でfirebase に接続できるように修正

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAAoxZFeX-ZJSstCPuHn-7s7taksH_UKCM",
+    authDomain: "companyboard-6ebe3.firebaseapp.com",
+    projectId: "companyboard-6ebe3",
+    storageBucket: "companyboard-6ebe3.appspot.com",
+    messagingSenderId: "468730701426",
+    appId: "1:468730701426:web:26980c9f8e0f7531aa04eb",
+    measurementId: "G-14HTS64W8W"
+  }
 };
